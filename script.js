0bb8a7f06e2f2a69d418d5290fe4b6a502a9a0df
mrslvs <sekerka.miroslav@outlook.com>
2022-08-08 19:35:55 +0200

    changed naming from Item to Task

--- script.js
+++ script.js
@@ -1,79 +1,109 @@
 "use strict";
 
+// to-do
+//      types of tasks (home, work, sport, ...)
+//      do not add duplicates
+
 const form = document.getElementById("form");
 const formInputText = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
 
 class App {
-    items = [];
-    finishedItems;
+    tasks = [];
+    finishedTasks = [];
 
     constructor() {
         // load item from application data
-        this._loadItems();
+        this._loadTasks();
 
-        // handle submitting new item
-        form.addEventListener("submit", this._addItem.bind(this));
+        // handle submitting new task
+        form.addEventListener("submit", this._addTask.bind(this));
+
+        // handle button click (finish/delete task)
+        document.addEventListener("click", function (e) {
+            const clicked = e.target;
+
+            if (clicked.classList.contains("task-button")) {
+                // get .todo-text (sibling element)
+                //      1. choose parent element
+                //      2. search for the child by class (.todo-text)
+                const taskText =
+                    clicked.parentElemet.querySelector(".todo-text");
+
+                // get instance by searching for text (assuming no duplicate tasks are present)
+
+                // finishing or deleting? :
+                if (clicked.classList.contains("finish-task")) {
+                }
+
+                if (clicked.classList.contains("delete-task")) {
+                }
+            }
+        });
     }
 
-    _addItem(e) {
+    _addTask(e) {
         // prevent page from reloading
         e.preventDefault();
 
         // create instance of Item if text is entered
         if (formInputText.value.length === 0) return;
-        const item = new Item(formInputText.value);
+        const task = new Task(formInputText.value);
 
         // save item to local storage
-        this.items.push(item);
-        localStorage.setItem("todo-items", JSON.stringify(this.items));
+        this.tasks.push(task);
+        localStorage.setItem("todo-tasks", JSON.stringify(this.tasks));
 
         // display item
-        this._displayItem(item);
+        this._displayTask(task);
 
         // clear input field
         formInputText.value = "";
         formInputText.focus();
     }
 
-    _displayItem(item) {
+    _displayTask(task) {
         let html = `
-            <div class="todo-item">
-                <p class="todo-text">${item.text}</p>
-                <button class="item-button">✅</button>
-                <button class="item-button">❌</button>
+            <div class="todo-task">
+                <p class="todo-text">${task.text}</p>
+                <button class="task-button finish-task">✅</button>
+                <button class="task-button delete-task">❌</button>
             </div>
         `;
 
         list.insertAdjacentHTML("afterbegin", html);
     }
 
-    _loadItems() {
-        const items = JSON.parse(localStorage.getItem("todo-items"));
+    _loadTasks() {
+        const tasks = JSON.parse(localStorage.getItem("todo-tasks"));
 
-        if (!items) return;
+        if (!tasks) return;
 
-        this.items = items;
+        this.tasks = tasks;
 
-        this.items.forEach(item => {
-            this._displayItem(item);
+        this.tasks.forEach(task => {
+            this._displayTask(task);
         });
     }
 
-    _finishTask(item) {
-        item.finishTask();
+    _finishTask(task) {
+        task.finishTask();
 
-        // from array remove passed item
-        this.items.splice(this.items.indexOf(item), 1);
-        this.finishedTasks.push(item);
+        // from array remove passed task
+        this.tasks.splice(this.task.indexOf(task), 1);
+        this.finishedTasks.push(task);
 
         // update local storage
-        localStorage.removeItem("todo-items");
-        localStorage.setItem("todo-items", JSON.stringify(this.items));
+        localStorage.removeItem("todo-tasks");
+        localStorage.setItem("todo-tasks", JSON.stringify(this.tasks));
+        localStorage.setItem(
+            "completed_tasks",
+            JSON.stringify(this.finishedTasks)
+        );
     }
 }
 
-class Item {
+class Task {
     id;
     text;
     createdDate;
@@ -83,11 +113,10 @@ class Item {
         this.id = (Date.now() + "").slice(-10); // last 10 digits
         this.createdDate = Date.now();
         this.text = text + "";
-        console.log("created instance of Item");
     }
 
     finishTask() {
-        this.this.finishedDate = Date.now();
+        this.finishedDate = Date.now();
         this.text = this.text.strike();
     }
 }
