58a60c2e784cab1366805336cca0153024c5ebc9
mrslvs <sekerka.miroslav@outlook.com>
2022-08-08 15:26:42 +0200

    created classes App & Item

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const form = document.getElementById("form");
+const formInputText = document.getElementById("todo-input");
+
+class App {
+    constructor() {
+        // load item from application data
+        form.addEventListener("submit", this._addItem);
+        console.log("created instance of App");
+    }
+
+    _addItem(e) {}
+}
+
+class Item {
+    text;
+    createdDate;
+    finishedDate;
+
+    constructor(text) {
+        this.createdDate = Date.now();
+        this.text = text + "";
+    }
+
+    _finishTask() {
+        this.finishedDate = Date.now();
+    }
+}
+
+const app = new App();
