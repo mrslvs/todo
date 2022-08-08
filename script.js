ad6c5313478be5a4680ea2b848f5945d9bf23678
mrslvs <sekerka.miroslav@outlook.com>
2022-08-08 16:16:35 +0200

    added function to display to-do item

--- script.js
+++ script.js
@@ -2,15 +2,41 @@
 
 const form = document.getElementById("form");
 const formInputText = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
 
 class App {
     constructor() {
         // load item from application data
-        form.addEventListener("submit", this._addItem);
+        form.addEventListener("submit", this._addItem.bind(this));
         console.log("created instance of App");
     }
 
-    _addItem(e) {}
+    _addItem(e) {
+        // prevent page from reloading
+        e.preventDefault();
+
+        // create instance of Item if text is entered
+        if (formInputText.value.length === 0) return;
+
+        const item = new Item(formInputText.value);
+
+        //save item to local storage
+
+        //display item (refresh)
+        this._displayItem(item);
+    }
+
+    _displayItem(item) {
+        let html = `
+            <div class="todo-item">
+                <p class="todo-text">${item.text}</p>
+                <button class="item-button">✅</button>
+                <button class="item-button">🚮</button>
+            </div>
+        `;
+
+        list.insertAdjacentHTML("afterbegin", html);
+    }
 }
 
 class Item {
@@ -21,6 +47,7 @@ class Item {
     constructor(text) {
         this.createdDate = Date.now();
         this.text = text + "";
+        console.log("created instance of Item");
     }
 
     _finishTask() {
